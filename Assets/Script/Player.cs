e9640e67d4a615537d7f8dcd73147a637abdcfb5
Radon0 <96102766+Radon0@users.noreply.github.com>
2022-12-19 15:04:51 +0900

    プレイヤーの動き

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] float moveSpeed;
+
+    bool isMoving;
+    Vector2 input;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,38 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       //動いていない時
+       if(!isMoving)
+        {
+            //キーボード入力を受け付ける
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            //入力があったら
+            if(input != Vector2.zero)
+            {
+                //入力分を追加
+                Vector2 targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+                StartCoroutine(Move(targetPos));
+            }
+        }
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        //targetと現在のpositionの差がある間は、MoveTowardsでtargetPosに近く
+        while((targetPos - transform.position).sqrMagnitude>Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
     }
 }
