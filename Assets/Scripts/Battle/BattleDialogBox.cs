ddf4679c2e6edbdda3d96d52eb8b13b2a75bf582
Radon0 <96102766+Radon0@users.noreply.github.com>
2022-12-24 18:06:27 +0900

    メッセージテキスト

--- Assets/Scripts/Battle/BattleDialogBox.cs
+++ Assets/Scripts/Battle/BattleDialogBox.cs
@@ -9,6 +9,16 @@ public class BattleDialogBox : MonoBehaviour
     [SerializeField] int letterPerSecond;  //1文字当たりの時間
     [SerializeField] Text dialogText;
 
+    [SerializeField] GameObject actionSelector;
+    [SerializeField] GameObject skillSelector;
+    [SerializeField] GameObject skillDetails;
+
+    [SerializeField] List<Text> actionTexts;
+    [SerializeField] List<Text> skillTexts;
+
+    [SerializeField] Text ppText;
+    [SerializeField] Text typeText;
+
     //変更するための関数
     public void SetDialog(string dialog)
     {
@@ -24,5 +34,24 @@ public class BattleDialogBox : MonoBehaviour
             dialogText.text += letter;
             yield return new WaitForSeconds(1f / letterPerSecond);
         }
+    }
+
+    //UIの表示/非表示
+
+    //dialogTextの表示管理
+    public void EnableDialogText(bool enabled)
+    {
+        dialogText.enabled = enabled;
+    }
+    //actionSelectorの表示管理
+    public void EnableActionSelector(bool enabled)
+    {
+        actionSelector.SetActive(enabled);
+    }
+    //skillSelectorの表示管理
+    public void EnableSkillSelector(bool enabled)
+    {
+        skillSelector.SetActive(enabled);
+        skillDetails.SetActive(enabled);
     }
 }
--- Assets/Scripts/Battle/BattleSystem.cs
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -12,13 +12,31 @@ public class BattleSystem : MonoBehaviour
 
     private void Start()
     {
+        StartCoroutine(SetupBattle());
+    }
+
+    IEnumerator SetupBattle()
+    {
         //ÉÇÉìÉXÉ^Å[ÇÃê∂ê¨Ç∆ï`âÊ
-        playerUnit.SetUp();  
+        playerUnit.SetUp();
         enemyUnit.SetUp();
         //HUDÇÃï`âÊ
         playerHud.SetData(playerUnit.Pokemon);
         enemyHud.SetData(enemyUnit.Pokemon);
 
-        StartCoroutine(dialogBox.TypeDialog($"A wild {enemyUnit.Pokemon.Base.name} appeared."));
+        yield return dialogBox.TypeDialog($"A wild {enemyUnit.Pokemon.Base.name} appeared.");
+        yield return new WaitForSeconds(1);
+        dialogBox.EnableActionSelector(true);
     }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            dialogBox.EnableDialogText(false);
+            dialogBox.EnableActionSelector(false);
+            dialogBox.EnableSkillSelector(true);
+        }
+    }
+
 }
